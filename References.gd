e660c9b1377d8f510e0475661c28bbb7e772aa27
Regonold <regonold520@gmail.com>
2026-07-22 19:41:27 +0100

    Added basic machine caching + datastore

--- References.gd
+++ References.gd
@@ -1,1 +1,20 @@
 extends Node
+
+@onready var machineData : Dictionary = parse_json_path("res://datastore/machines.json")
+
+
+
+
+
+
+func parse_json_path(path):
+	var file := FileAccess.open(path, FileAccess.READ)
+	if file:
+		var json_text := file.get_as_text()
+		file.close()
+		var result = JSON.parse_string(json_text)
+		if result != null:
+			var my_dict : Dictionary = result
+			return my_dict
+		else:
+			push_error("Failed to parse JSON")
